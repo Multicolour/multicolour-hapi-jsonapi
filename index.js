8ff09019311fb4e8a5f1cfb0e8b65fea7da840c4
davemackintosh <me@davemackintosh.co.uk>
2015-11-12 19:12:37 +0000

    Added docblocks
    Signed-off-by: davemackintosh <me@davemackintosh.co.uk>

--- index.js
+++ index.js
@@ -2,6 +2,11 @@
 
 class Multicolour_Hapi_JSONAPI extends Map {
 
+  /**
+   * Set some default options.
+   * @param  {Multicolour_Server_Hapi} generator using this plugin.
+   * @return {Multicolour_Hapi_JSONAPI} Multicolour_Hapi_JSONAPI for chaining.
+   */
   constructor(generator) {
     // Construct.
     super()
@@ -10,8 +15,16 @@ class Multicolour_Hapi_JSONAPI extends Map {
     this
       .set("decorator_name", "jsonapi")
       .set("generator", generator.request("host"))
+
+    return this
   }
 
+  /**
+   * This is the actual decorator for the Hapi server.
+   * @param  {Array|Object} results from a Waterline query.
+   * @param  {Waterline.Collection} collection the results are from.
+   * @return {Hapi.Response} Hapi's response object for chaining.
+   */
   generate_payload(results, collection) {
     // Get the JSON API formatter.
     const JSONAPIModel = require("waterline-jsonapi")
@@ -20,27 +33,26 @@ class Multicolour_Hapi_JSONAPI extends Map {
     return this.response(JSONAPIModel.create(results, collection).toJSON())
   }
 
+  /**
+   * Register the plugin with the Multicolour server.
+   * @return {Multicolour_Hapi_JSONAPI} Multicolour_Hapi_JSONAPI for chaining.
+   */
   register() {
-    // Get the server.
+    // Get the server and decorator name.
     const generator = this.get("generator")
     const server = generator.request("raw")
     const name = this.get("decorator_name")
 
+    // Set the new decorator name.
     generator.reply("decorator", name)
 
     // Decorate the reply.
     server.decorate("reply", name, this.generate_payload)
 
+    // Return the calling generator.
     return generator
   }
 }
 
-// Export the required config for Multicolour
-// to register and handle.
-module.exports = {
-  // It's an auth plugin.
-  type: require("multicolour/lib/consts").SERVER_TRANSFORMER,
-
-  // The generator is the class above.
-  plugin: Multicolour_Hapi_JSONAPI
-}
+// Export the plugin.
+module.exports = Multicolour_Hapi_JSONAPI
