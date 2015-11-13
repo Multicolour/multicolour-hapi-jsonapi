b6f89475cc9e514bafb53aca766bcc90e79c92bd
davemackintosh <me@davemackintosh.co.uk>
2015-11-13 11:28:52 +0000

    Payloads validation, headers
    Updated to new Multicolour plugin architecture
    Added incoming and outgoing payload validation.
    
    Signed-off-by: davemackintosh <me@davemackintosh.co.uk>

--- index.js
+++ index.js
@@ -1,5 +1,47 @@
 "use strict"
 
+// Get our tools.
+const joi = require("joi")
+const waterline_joi = require("waterline-joi")
+
+/**
+ * Shallow clone the provided object.
+ * @param  {Object} attributes to clone.
+ * @return {Object} cloned object.
+ */
+function clone_attributes(attributes) {
+  return JSON.parse(JSON.stringify(attributes))
+}
+
+/**
+ * Check over the associations in the collection
+ * and fix them for the payloads.
+ * @param  {Object} attributes from a collection.
+ * @param  {String} type, either "string" or "object"
+ * @return {Object} Fixed object.
+ */
+function check_and_fix_associations(attributes, type) {
+  // If it's a string, remove erroneous keys.
+  if (type === "string") {
+    delete attributes.id
+    delete attributes.createdAt
+    delete attributes.updatedAt
+  }
+
+  // Loop over the attributes to see if we have
+  // any relationship type fields to add validation for.
+  for (const attribute in attributes) {
+    if (attributes[attribute].hasOwnProperty("model")) {
+      attributes[attribute] = type
+    }
+    else if (attributes[attribute].hasOwnProperty("collection")) {
+      attributes[attribute] = type === "object" ? "array" : type
+    }
+  }
+
+  return attributes
+}
+
 class Multicolour_Hapi_JSONAPI extends Map {
 
   /**
@@ -20,6 +62,54 @@ class Multicolour_Hapi_JSONAPI extends Map {
   }
 
   /**
+   * Get the read only schema for a collection.
+   * @param  {Waterline.Collection} collection to get payload for.
+   * @return {Joi.Schema} Schema for any requests.
+   */
+  get_response_schema(collection) {
+    // Clone the attributes.
+    const attributes = clone_attributes(collection._attributes)
+
+    // Generate a Joi schema from a fixed version of the attributes.
+    const payload = waterline_joi(check_and_fix_associations(attributes, "object"))
+
+    return joi.alternatives().try(
+      joi.object({
+        data: joi.alternatives().try(joi.array().items(payload), payload),
+        links: joi.object({
+          self: joi.string(),
+          last: joi.string(),
+          next: joi.string()
+        })
+      }),
+      joi.object({
+        errors: joi.alternatives().try(joi.array(), joi.object())
+      })
+    )
+  }
+
+  /**
+   * Get the schema for write operations.
+   * @param  {Waterline.Collection} collection to get payload for.
+   * @return {Joi.Schema} Schema for any requests.
+   */
+  get_payload_schema(collection) {
+    // Get our tools.
+    const extend = require("util")._extend
+    const attributes = clone_attributes(collection._attributes)
+
+    // Extend our attributes over some Waterline defaults.
+    extend({
+      id: collection._attributes.id,
+      createdAt: collection._attributes.createdAt,
+      updatedAt: collection._attributes.updatedAt
+    }, attributes)
+
+    // Return the schema.
+    return waterline_joi(check_and_fix_associations(attributes, "string"))
+  }
+
+  /**
    * This is the actual decorator for the Hapi server.
    * @param  {Array|Object} results from a Waterline query.
    * @param  {Waterline.Collection} collection the results are from.
@@ -37,11 +127,25 @@ class Multicolour_Hapi_JSONAPI extends Map {
    * Register the plugin with the Multicolour server.
    * @return {Multicolour_Hapi_JSONAPI} Multicolour_Hapi_JSONAPI for chaining.
    */
-  register() {
+  register(Multicolour_Server_Hapi) {
     // Get the server and decorator name.
     const generator = this.get("generator")
     const server = generator.request("raw")
     const name = this.get("decorator_name")
+
+    // Register with the server some properties it requires.
+    Multicolour_Server_Hapi
+      // Set it's validator to this plugin.
+      .set("validator", this)
+      // Set the response and payload validators to this plugin's.
+      .reply("response_schema", this.get_response_schema.bind(this))
+      .reply("payload_schema", this.get_payload_schema.bind(this))
+      // Update the accept header to the one in the spec.
+      .request("header_validator")
+        .set("accept", joi.string()
+          .valid("application/vnd.api+json")
+          .default("application/vnd.api+json")
+          .required())
 
     // Set the new decorator name.
     generator.reply("decorator", name)
